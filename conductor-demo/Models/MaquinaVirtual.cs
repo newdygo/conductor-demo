4a53ceb0a35b7da2e0bba251f9f2eebe970a4a73
Diego Leonardo Balduino <dbalduino.sup@conductoraz.onmicrosoft.com>
2017-11-14 14:57:21 -0200

    Definição do modelo de  negócio da aplicação

---
+++ conductor-demo/Models/MaquinaVirtual.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace conductor.demo.Models
+{
+    /// <summary>
+    /// Define o modelo da máquina virtual.
+    /// </summary>
+    public class MaquinaVirtual
+    {
+        #region Propriedades
+
+        /// <summary>
+        /// Nome da máquina virtual.
+        /// </summary>
+        public string Nome { get; set; }
+
+        /// <summary>
+        /// Descricao da maáquina virtual
+        /// </summary>
+        public string Descricao { get; set; }
+
+        #endregion
+    }
+}
