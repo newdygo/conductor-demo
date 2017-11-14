d3e2550108849eda1abd212e76079de17aac8ce1
Diego Leonardo Balduino <dbalduino.sup@conductoraz.onmicrosoft.com>
2017-11-14 15:06:53 -0200

    Criação do context da daplicação com o banco de dados

---
+++ conductor-demo/Data/ConductorDemoContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using conductor.demo.Models;
+
+    public class ConductorDemoContext : DbContext
+    {
+        public ConductorDemoContext (DbContextOptions<ConductorDemoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<conductor.demo.Models.MaquinaVirtual> MaquinaVirtual { get; set; }
+    }
