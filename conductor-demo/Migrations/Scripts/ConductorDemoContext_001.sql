00f000bd5e9aeb4e76198245b93485b9a58a5b0b
Diego Leonardo Balduino <dbalduino.sup@conductoraz.onmicrosoft.com>
2017-11-14 15:28:08 -0200

    Scripts de migração do banco

---
+++ conductor-demo/Migrations/Scripts/ConductorDemoContext_001.sql
@@ -0,0 +1,25 @@
+﻿IF OBJECT_ID(N'__EFMigrationsHistory') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+
+GO
+
+CREATE TABLE [MaquinaVirtual] (
+    [Id] int NOT NULL IDENTITY,
+    [Descricao] nvarchar(max) NULL,
+    [Nome] nvarchar(max) NULL,
+    CONSTRAINT [PK_MaquinaVirtual] PRIMARY KEY ([Id])
+);
+
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20171114171525_001', N'2.0.0-rtm-26452');
+
+GO
+
